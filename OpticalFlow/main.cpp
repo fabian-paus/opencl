5ad05b103ce4dfc05fb00c5eac7f1d01e3ab01a9
fabian-paus <paus.fabian@gmail.com>
2015-03-20 20:29:30 +0100

    Initial commit for project "OpticalFlow"

---
+++ OpticalFlow/main.cpp
@@ -0,0 +1,12 @@
+#include <CL/cl.hpp>
+
+#include <boost/gil/image.hpp>
+#include <boost/gil/extension/io/jpeg_io.hpp>
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World!\n";
+	return 0;
+}
